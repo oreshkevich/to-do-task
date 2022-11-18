2af946f91755f30a4d7f32b166f9982ac289dfaa
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-11-18 21:28:11 +0300

    fix: ToDo toggleTask

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,13 +1,6 @@
 function ToDo({ todo, toggleTask, removeTask }) {
   return (
     <div key={todo.id} className="item-todo">
-      <div
-        className={todo.complete ? 'item-text strike' : 'item-text'}
-        onClick={() => toggleTask(todo.id)}
-      >
-        <span>Название: </span>
-        {todo.nameTask}
-      </div>
       <button className="item-delete" onClick={() => removeTask(todo.id)}>
         X
       </button>
@@ -15,22 +8,23 @@ function ToDo({ todo, toggleTask, removeTask }) {
         className={todo.complete ? 'item-text strike' : 'item-text'}
         onClick={() => toggleTask(todo.id)}
       >
-        <span>Описание: </span>
-        {todo.descriptionTask}
-      </div>
-      <div
-        className={todo.complete ? 'item-text strike' : 'item-text'}
-        onClick={() => toggleTask(todo.id)}
-      >
-        <span>Дата: </span>
-        {todo.completionDate}
-      </div>
-      <div
-        className={todo.complete ? 'item-text strike' : 'item-text'}
-        onClick={() => toggleTask(todo.id)}
-      >
-        <span>Документ: </span>
-        {todo.image}
+        <div>
+          <span>Название: </span>
+          {todo.nameTask}
+        </div>
+
+        <div>
+          <span>Описание: </span>
+          {todo.descriptionTask}
+        </div>
+        <div>
+          <span>Дата: </span>
+          {todo.completionDate}
+        </div>
+        <div>
+          <span>Документ: </span>
+          {todo.image}
+        </div>
       </div>
     </div>
   );
